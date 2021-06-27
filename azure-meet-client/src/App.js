d57dd46e7cb8d79f2e0289f155126d5c1d93ab36
Jean <jeanlucafp@gmail.com>
2021-06-27 14:15:47 -0300

    concluido

--- azure-meet-client/src/App.js
+++ azure-meet-client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import './App.css'
 
 function App() {
+  const jitsiContainerId = 'jitsi-container-id'
+
+  const start = () => {
+    const domain = 'meet.jit.si'
+    const options = {
+      roomName: 'AzureMeetClient',
+      height: 1000,
+      parentNode: document.getElementById(jitsiContainerId)
+    }
+
+    const api = new window.JitsiMeetExternalAPI(domain, options)
+    api.addEventListener('videoConferenceJoined', () => {
+      console.log('usuario entrou')
+      api.executeCommand('displayName', 'Jean')
+    })
+  }
+
+  useEffect(() => {
+    if (window.JitsiMeetExternalAPI) {
+      start()
+    } else {
+      alert('jitsi ainda nao careregou')
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id={jitsiContainerId} className="jitsi" />
     </div>
   );
 }
